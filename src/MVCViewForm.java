31bd559d1bbaf61271fa49e1a35fa8ef79b0abe8
MilkyasEyob.Habte <milkyas23>
2020-11-10 13:41:47 +0100

    Initial commit

---
+++ src/MVCViewForm.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class MVCViewForm {
+    private JPanel MVCpannel;
+    private JButton publishButton;
+    private JTextArea programmeringTextArea;
+    private JTable table1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MVCViewForm");
+        frame.setContentPane(new MVCViewForm().MVCpannel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+    private void createUIComponents() {
+
+        // TODO: place custom component creation code here
+    }
+}
